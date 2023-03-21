9cbcd4be7fd46c7ebfa3efb762c215929b3dddac
Riley Grotenhuis <rg@buildmidwestern.com>
2023-03-21 15:29:52 -0500

    feat: add see more buttons

--- components/TopArtists.js
+++ components/TopArtists.js
@@ -1,15 +1,20 @@
 import styles from './TopArtists.module.css';
+import Link from 'next/link';
 import ArtistBlock from "./ArtistBlock";
 
 export default function TopArtists() {
     return (
         <div className={styles.TopArtistContainer}>
-            <h5 className={styles.TopArtistTitle}>Top Artists of All Time</h5>
+            <div className={styles.TopArtistTextContainer}>
+                <h5 className={styles.TopArtistTitle}>Top Artists of All Time</h5>
+                <Link href='/artists'>
+                    <p className={styles.TopArtistSeeMore}>See More</p>
+                </Link>
+            </div>
             <div className={styles.TopArtistBlockContainer}>
                 <ArtistBlock />
                 <ArtistBlock />
                 <ArtistBlock />
-                
             </div>
         </div>
     );
--- components/TopSongs.js
+++ components/TopSongs.js
@@ -1,10 +1,16 @@
 import styles from './TopSongs.module.css';
+import Link from 'next/link';
 import SongBlock from './SongBlock';
 
 export default function TopSongs() {
     return (
         <div className={styles.TopSongContainer}>
-            <h5 className={styles.TopSongTitle}>Top Tracks of All Time</h5>
+            <div className={styles.TopSongTextContainer}>
+                <h5 className={styles.TopSongTitle}>Top Songs of All Time</h5>
+                <Link href='/songs'>
+                    <p className={styles.TopSongSeeMore}>See More</p>
+                </Link>
+            </div>
             <div className={styles.TopSongBlockContainer}>
                 <SongBlock />
                 <SongBlock />
