eafa9df704f01204a84a4bfbe5e7e7e43ead93ef
Riley Grotenhuis <rg@buildmidwestern.com>
2023-03-21 16:36:42 -0500

    fix: add redirect code to all other pages

--- pages/songs.js
+++ pages/songs.js
@@ -1,3 +1,23 @@
+export function getServerSideProps(context) {
+    const { req } = context;
+    const spotifyAccessToken = req.cookies.spotify_access_token ?? null;
+  
+    if (!spotifyAccessToken) {
+      return {
+        redirect: {
+          destination: '/login',
+          permanent: false
+        }
+      };
+    }
+  
+    return {
+      props: {
+        spotifyAccessToken
+      }
+    };
+  };
+
 export default function Songs() {
     return (
         <div>
