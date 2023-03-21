b7d5435c48c0b47ea52e4cd8f30b4bab986075e6
Riley Grotenhuis <rg@buildmidwestern.com>
2023-03-21 16:04:32 -0500

    feat: redirect to login page if no access token is found

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,30 @@
+import cookie from 'cookie';
 import ProfileDetails from "../components/ProfileDetails";
 import TopArtists from "../components/TopArtists";
 import TopSongs from "../components/TopSongs";
 import AllPlaylists from "../components/AllPlaylists";
 
-export default function Home() {
+export function getServerSideProps(context) {
+  const cookies = cookie.parse(context.req.headers.cookie);
+  const spotifyAccessToken = cookies.spotify_access_token ?? null;
+
+  if (!spotifyAccessToken) {
+    return {
+      redirect: {
+        destination: '/login',
+        permanent: false
+      }
+    };
+  }
+
+  return {
+    props: {
+      spotifyAccessToken
+    }
+  };
+};
+
+export default function Home(props) {
   return (
     <div>
       <ProfileDetails />
